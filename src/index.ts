39bb976ec4179d38d913ddd9307eaf86c42f4a35
my1000ludens <companyrun0@gmail.com>
2022-10-22 21:00:40 +0900

    feat: 인스타 아이콘 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world!!!')
+const Icon = document.createElement('a')
+Icon.className = 'icon-instagram'
+Icon.href = 'https://www.instagram.com/my1000ludens_official'
+Icon.target = '_blank'
+document.body.appendChild(Icon)
